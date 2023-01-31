02663a2d7bb578cc99d3a7915cd091f1739935aa
adityasharmadevops <aditya.sharma287@yahoo.com>
2023-01-31 14:17:27 +0530

    added apm tracer

--- shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplication.java
+++ shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplication.java
@@ -1,5 +1,6 @@
 package uk.co.danielbryant.djshopping.shopfront;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -19,3 +20,23 @@ public class ShopfrontApplication {
         return new RestTemplate();
     }
 }
+
+Tracer tracer = new ApmTracer.Builder("mytraceA", "APM RUM PROJECT")
+                    .withMicrosecondAccurateTimestamp(true)
+                    .withCollectMetrics(true)
+                    .withCollectResources(true)
+                    .withDataUploadEndpoint(https://aaaadboo7t2z4aaaaaaaaabzjq.apm-agt.us-ashburn-1.oci.oraclecloud.com)
+                    .withProperty("com.oracle.apm.agent.private.data.key", "BDEHKOB55OD4HY4RMACS35YFA4J5GSYU")
+                    
+                    .withProperty("com.oracle.apm.agent.data.upload.file","observations.log")
+                    .withProperty("com.oracle.apm.agent.data.upload.proxy.url","http://www-proxy-hqdc.us.oracle.com:80")
+                    .build();
+            System.out.println("tracer created:" + ((ApmTracer) tracer).getServiceName());
+            //GlobalTracer.register(tracer);
+            Span span = tracer.buildSpan("myspan_withdelay")
+                    .start();
+            
+
+            Thread.sleep(2000);
+
+            span.finish();
